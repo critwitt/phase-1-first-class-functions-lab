f7cbe4ab494212296e14643f85bea4d437c99510
Chris Witte <christopher.t.witte@gmail.com>
2022-08-08 23:41:24 -0400

    commit tree 0d332ac703d56f982120f7bcfe2c76296125bc61

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+    return arr.slice(0, 2)
+}
+
+function returnLastTwoDrivers(arr) {
+    return arr.slice(arr.length-2, arr.length)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare*multiplier
+    }
+}
+
+function fareDoubler(fare) {
+    const doubledFare = createFareMultiplier(2);
+    return doubledFare(fare)
+}
+
+function fareTripler(fare) {
+    const tripledFare = createFareMultiplier(3);
+    return tripledFare(fare)
+}
+
+function selectDifferentDrivers (allDrivers, selectedDrivers) {
+    return selectedDrivers(allDrivers)
+}
